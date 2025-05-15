42981eeed2578a285d082a27fe4d7ae4813fac2c
dia24 <dia24@estsoft.com>
2025-05-15 17:04:18 +0900

    chore: tailwindcss 설치 및 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="app-welcome">
+    <div className="app-welcome bg-gradient-to-r from-blue-400 to-purple-500 text-white p-8 rounded-xl shadow-lg">
       Welcome
     </div>
   )
